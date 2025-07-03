9acdd8e736ec80775bfbbf79b41c8aca33829f45
Hadi Zaidi <38956768+0xVR@users.noreply.github.com>
2025-07-03 00:36:03 -0400

    logic >>> wire

--- src/top_fft.sv
+++ src/top_fft.sv
@@ -2,7 +2,7 @@ module tt_um_FFT_engine (
     input  wire [7:0] ui_in,
     output logic [7:0] uo_out,
     input  wire [7:0] uio_in,
-    output wire [7:0] uio_out,
+    output logic [7:0] uio_out,
     output logic [7:0] uio_oe,
     input  wire       ena,
     input  wire       clk,
